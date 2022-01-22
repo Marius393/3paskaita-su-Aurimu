aa1ca061a59754564b61580885830f483dd1547d
MARIUS MOCKUS <mariusmockus93@gmail.com>
2022-01-22 14:43:58 +0200

    last commit

---
+++ script2.js
@@ -0,0 +1,8 @@
+
+
+function sumOfTwoNumbers(skaiciusPirmas, skaiciusAntras) {
+   return skaiciusPirmas + skaiciusAntras;
+        
+}
+const atsakymas = sumOfTwoNumbers (4, 6 );
+alert(atsakymas);
---
+++ script3.js
@@ -0,0 +1,20 @@
+const celsius = prompt('Įveskite laipsnius');
+
+
+const atsakymas = cToF(celsius);
+
+alert(atsakymas)
+
+function cToF(celsius) {
+    return celsius * 1.8 + 32;
+}
+
+// celsius i fahrenheit
+
+
+
+eur i rublius
+
+function eurToRub(eur) {
+    return eur * 88.05;
+}
